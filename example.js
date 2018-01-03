cf13acbec8225ad5567edd5c79057d18cc5cb53d
Michael Williams <michael.williams@enspiral.com>
2018-01-03 23:14:57 +1300

    almost a thing!

--- example.js
+++ example.js
@@ -1,9 +1,74 @@
 var fs = require('fs')
 var Path = require('path')
+var pull = require('pull-stream')
 var ssbConfig = require('ssb-config/inject')
 var ssbKeys = require('ssb-keys')
 
 console.log('STARTING SBOT')
+var Bot = require('./')
+
+var bot = Bot({
+  name: 'hello',
+  version: '0.0.0',
+  flumeVersion: 2,
+  initBot: (sbot) => {
+    var { id } = sbot.whoami()
+    var hasMentionForBot = hasMentionFor(id)
+
+    return {
+      handleMessage,
+      handleAction
+    }
+
+    function handleMessage (state, message) {
+      if (hasMentionForBot(message)) {
+        return {
+          state,
+          action: {
+            type: 'hello',
+            id: message.value.author
+          }
+        }
+      }
+
+      return { state }
+    }
+
+    function handleAction (action, sbot, cb) {
+      var { type } = action
+      if (type === 'hello') {
+        var { id } = action
+        sbot.about.get((err, abouts) => {
+          if (err) return cb(err)
+          var name = abouts[id].name[id][0]
+          cb(null, {
+            type: 'post',
+            text: `hello [@${name}](${id})!`,
+            mentions: [
+              {
+                name,
+                link: id
+              }
+            ]
+          })
+        })
+      }
+      else cb()
+    }
+  }
+})
+
+function hasMentionFor (id) {
+  return (message) => {
+    var { value } = message
+    var { content } = value
+    var { mentions = [] } = content
+    return mentions.map(mention => mention.link).includes(id)
+  }
+}
+
+// ---
+// boilerplate
 
 var createServer = require('scuttlebot')
   .use(require('scuttlebot/plugins/gossip'))
@@ -16,7 +81,7 @@ var createServer = require('scuttlebot')
   .use(require('scuttlebot/plugins/local'))
   .use(require('scuttlebot/plugins/logging'))
   .use(require('ssb-about'))
-  .use(require('./'))
+  .use(bot)
 
 var appName = 'ssb-bot'
 
@@ -35,4 +100,23 @@ var keys = ssbKeys.loadOrCreateSync(
 
 config.keys = keys
 
-var server = createServer(config)
+var sbot = createServer(config)
+var { id } = sbot.whoami()
+
+console.log('bot id:', id)
+
+sbot.replicate.request(
+  '@6ilZq3kN0F+dXFHAPjAwMm87JEb/VdB+LC9eIMW3sa0=.ed25519',
+  (err) => {
+    if (err) throw err
+    console.log('done replicate.request')
+  }
+)
+
+pull(
+  sbot.createUserStream({
+    id,
+    live: true
+  }),
+  pull.log()
+)
--- index.js
+++ index.js
@@ -1,173 +1,126 @@
-const pull = require('pull-stream')
-const FlumeviewReduce = require('flumeview-reduce')
-const pullThrough = require('pull-through')
+var pull = require('pull-stream')
+var FlumeviewReduce = require('flumeview-reduce')
+var pullThrough = require('pull-through')
 
-module.exports = {
-  name: 'bot',
-  version: '0.0.0',
-  manifest: {},
-  init
-}
+module.exports = Bot
 
-const version = 1
+function Bot (options) {
+  var {
+    name = 'bot',
+    version = '0.0.0',
+    initBot = () => ({
+      handleMessage: (state, message) => ({ state: null }),
+      handleAction: (action, sbot, cb) => cb()
+    }),
+    flumeVersion = 1
+  } = options
 
-function hasMentionFor (id) {
-  return (message) => {
-    const { value } = message
-    const { content } = value
-    const { mentions = [] } = content
-    return mentions.map(mention => mention.link).includes(id)
+  return {
+    name,
+    version,
+    manifest: {},
+    init
   }
-}
-
-function init (sbot) {
-  const { id } = sbot.whoami()
-  console.log('id:', id)
   
-  const hasMentionForBot = hasMentionFor(id)
-  
-  function update (state, message) {
-    // console.log('update', message)
+  function init (sbot) {
+    var {
+      handleMessage,
+      handleAction
+    } = initBot(sbot)
 
-    if (state == null) {
-      state = {}
-    }
+    var { id } = sbot.whoami()
 
-    var action = null
-
-    if (hasMentionForBot(message)) {
-      action = {
-        type: 'hello',
-        id: message.value.author
-      }
-    }
-
-    return { state, action }
-  }
-
-  function run (action, sbot, cb) {
-    const { type } = action
-    if (type === 'hello') {
-      const { id } = action
-      sbot.about.get((err, abouts) => {
-        if (err) return cb(err)
-        const name = abouts[id].name[id][0]
-        cb(null, {
-          type: 'post',
-          text: `hello [@${name}](${id})!`,
-          mentions: [
-            {
-              name,
-              link: id
-            }
-          ]
-        })
-      })
-    }
-    else cb()
-  }
-
-  function reduce (index, data) {
-    // console.log('reduce', index, data)
-    const { message, state, action } = data
-    const { key, value } = message
-    const { author, content } = value
-    // if message from bot self
-    if (author === id) {
-      const { branch: inReplyToId } = content
-      // if message is reply to
-      if (inReplyToId) {
-        if (index.actions[inReplyToId] !== undefined) {
-          delete index.actions[inReplyToId]
+    function reduce (index, data) {
+      // console.log('reduce', index, data)
+      var { message, state, action } = data
+      var { key, value } = message
+      var { author, content } = value
+      // if message from bot self
+      if (author === id) {
+        var { branch: inReplyToId } = content
+        // if message is reply to
+        if (inReplyToId) {
+          if (index.actions[inReplyToId] !== undefined) {
+            delete index.actions[inReplyToId]
+          }
         }
       }
-    }
-    // if message from somebody else
-    else {
-      index.state = data.state
-      if (action != null) index.actions[key] = action
-    }
-    return index
-  }
-
-  function map (message) {
-    // console.log('map', message)
-    if (typeof message.value.content === 'string') {
-      message = sbot.private.unbox(message) || message
-    }
-    // if message from bot self, return as is
-    if (message.value.key === id) return { message }
-    // if message not from bot, 
-    const value = index.value.value
-    const state = (value == null) ? null : value.state
-    const { state: nextState, action } = update(state, message)
-    return {
-      message,
-      state: nextState,
-      action
-    }
-  }
-  
-  const codec = null
-
-  const initial = {
-    state: null,
-    actions: {}
-  }
-
-  var index = sbot._flumeUse('bot', FlumeviewReduce(
-    version,
-    reduce,
-    map,
-    codec,
-    initial
-  ))
-
-  pull(
-    index.stream({
-      live: true
-    }),
-    pullThrough(function (data) {
-      const { actions } = data
-      if (actions) {
-        Object.keys(actions).forEach(messageId => {
-          const action = actions[messageId]
-          this.queue({ messageId, action })
-        })
+      // if message from somebody else
+      else {
+        index.state = data.state
+        if (action != null) index.actions[key] = action
       }
-    }),
-    pull.asyncMap(function ({ messageId, action }, cb) {
-      run(action, sbot, (err, content) => {
-        if (err) return cb(err)
-        sbot.get(messageId, (err, inReplyTo) => {
-          if (err) return cb(err)
-          content.root = inReplyTo.content.root || messageId
-          content.branch = messageId
-
-          cb(null, content)
-        })
-      })
-    }),
-    pull.asyncMap(function (content, cb) {
-      console.log('publishing!', content)
-      return cb()
-      sbot.publish(content, cb)
-    }),
-    pull.onEnd(err => {
-      if (err) throw err
-    })
-  )
-
-  pull(
-    sbot.createUserStream({ id }),
-    pull.log()
-  )
-  
-  sbot.replicate.request(
-    '@6ilZq3kN0F+dXFHAPjAwMm87JEb/VdB+LC9eIMW3sa0=.ed25519',
-    (err) => {
-      if (err) throw err
-      console.log('done replicate.request')
+      return index
     }
-  )
+
+    function map (message) {
+      // console.log('map', message)
+      if (typeof message.value.content === 'string') {
+        message = sbot.private.unbox(message) || message
+      }
+      // if message from bot self, return as is
+      if (message.value.key === id) return { message }
+      // if message not from bot, 
+      var value = index.value.value
+      var state = (value == null) ? null : value.state
+      console.log('handle message!', message)
+      var { state: nextState, action } = handleMessage(state, message)
+      console.log('action?', action)
+      return {
+        message,
+        state: nextState,
+        action
+      }
+    }
+
+    var codec = null
+
+    var initial = {
+      state: null,
+      actions: {}
+    }
+
+    var index = sbot._flumeUse(name, FlumeviewReduce(
+      flumeVersion,
+      reduce,
+      map,
+      codec,
+      initial
+    ))
+
+    pull(
+      index.stream({
+        live: true
+      }),
+      pullThrough(function (data) {
+        var { actions } = data
+        if (actions) {
+          Object.keys(actions).forEach(messageId => {
+            var action = actions[messageId]
+            this.queue({ messageId, action })
+          })
+        }
+      }),
+      pull.asyncMap(function ({ messageId, action }, cb) {
+        console.log('handle action!', action)
+        handleAction(action, sbot, (err, content) => {
+          if (err) return cb(err)
+          sbot.get(messageId, (err, inReplyTo) => {
+            if (err) return cb(err)
+            content.root = inReplyTo.content.root || messageId
+            content.branch = messageId
+
+            cb(null, content)
+          })
+        })
+      }),
+      pull.asyncMap(function (content, cb) {
+        console.log('publishing!', content)
+        sbot.publish(content, cb)
+      }),
+      pull.onEnd(err => {
+        if (err) throw err
+      })
+    )
+  }
 }
